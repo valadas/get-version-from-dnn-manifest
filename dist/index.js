966d6f98d9295899fdf933b86b7f7d38f50cb566
Daniel Valadas <info@danielvaladas.com>
2020-05-13 17:05:56 -0400

    Fetch first node of first file

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
+const glob = __importStar(require("@actions/glob"));
+const fs_1 = require("fs");
 async function run() {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const manifestPath = core.getInput('manifestPath');
         const packageName = core.getInput('packageName');
         if (manifestPath === "") {
@@ -18,9 +20,21 @@ async function run() {
                 core.setFailed("packageName can only be used if you also specify manifestPath.");
                 reject("packageName provided without a manifestPath");
             }
+            const globber = await glob.create(manifestPath, { followSymbolicLinks: false });
+            const files = await globber.glob();
+            const file = files[0];
+            console.log(getManifestVersion(file));
         }
         resolve();
     });
 }
+const getManifestVersion = (file, packageName = ".*") => {
+    var version = "";
+    const fileContent = fs_1.readFileSync(file).toString();
+    const rx = new RegExp(`<package.*name=".*${packageName}.*".*version="(.*)"`);
+    const node = rx.exec(fileContent);
+    console.log("Using package node: ", node);
+    return node ? node.toString() : "";
+};
 run();
 exports.default = run;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as core from '@actions/core';
+import * as glob from '@actions/glob';
+import { readFileSync } from 'fs';
 
 async function run() {
-    return new Promise<void>((resolve, reject) => {
+    return new Promise<void>(async (resolve, reject) => {
         const manifestPath = core.getInput('manifestPath');
         const packageName = core.getInput('packageName');
 
@@ -11,10 +13,22 @@ async function run() {
                 core.setFailed("packageName can only be used if you also specify manifestPath.");
                 reject("packageName provided without a manifestPath");
             }
-            
+            const globber = await glob.create(manifestPath, {followSymbolicLinks: false});
+            const files = await globber.glob();
+            const file = files[0];
+            console.log(getManifestVersion(file));
         }
         resolve();
     });
+}
+
+const getManifestVersion = (file: string, packageName: string = ".*", ):string => {
+    var version = "";
+    const fileContent = readFileSync(file).toString();
+    const rx = new RegExp(`<package.*name=".*${packageName}.*".*version="(.*)"`);
+    const node = rx.exec(fileContent);
+    console.log("Using package node: ", node);
+    return node ? node.toString() : "";
 }
 
 run();
