c690a24145ff5c709a0a7da61e61241141529410
Daniel Valadas <info@danielvaladas.com>
2020-05-13 15:00:58 -0400

    Commit @actions/core

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import * as core from '@actions/core';
+
 async function run() {
-    return new Promise<void>((resolve) => {
-        console.log("Test");
+    return new Promise<void>((resolve, reject) => {
+        const manifestPath = core.getInput('manifestPath');
+        const packageName = core.getInput('packageName');
+
+        if (manifestPath === "") {
+            console.log("No manifest path provided, will use the first package from the first manifest.");
+            if (packageName !== ""){
+                core.setFailed("packageName can only be used if you also specify manifestPath.");
+                reject("packageName provided without a manifestPath");
+            }
+            
+        }
         resolve();
     });
 }
